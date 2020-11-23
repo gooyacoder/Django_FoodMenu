563b970fe98d3d89c59e4efafa9053e91668187d
amirhosein <gooyacoder@gmail.com>
2020-11-23 16:58:48 +0330

    Upto 037

--- food/models.py
+++ food/models.py
@@ -5,6 +5,7 @@ class Item(models.Model):
 	item_name = models.CharField(max_length=200)
 	item_desc = models.CharField(max_length=200)
 	item_price = models.IntegerField()
+	item_image = models.CharField(max_length=500, default="https://lh3.googleusercontent.com/proxy/3slo6vQK0O5jchXrrArp1JocMXYyiGBZvFDwdd7hNfzOm0yvi9tnB9VDO4jEnvpXV3LOINboqWeKbiqxqtvFaAIgs7B5NmiCULF3DQ7mzNVQcnM5Obc8tD164xOk")
 
 	def __str__(self):
 		return self.item_name
