c0d53132c516ed3e15801e5aedb6b213a426c877
amirhosein <gooyacoder@gmail.com>
2020-11-27 21:31:18 +0330

    Upto 043

--- food/urls.py
+++ food/urls.py
@@ -7,5 +7,7 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:item_id>/', views.details, name='details'),
     path('add', views.create_item, name="create_item"),
+    path('update/<int:id>/', views.update_item, name="update_item"),
+    path('delete/<int:id>/', views.delete_item, name="delete_item"),
     
 ]
